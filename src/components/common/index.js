75325fe0eed40f52ffe2072631bc551ef89d82cb
Victoria Nguyen <nguyen.victoria2015@gmail.com>
2019-06-06 18:42:05 -0400

    Assignment 17

--- src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-//for both import and export in one line
-export * from './Button';
-export * from './Card';
-export * from './CardSection';
-export * from './Header';
-export * from './Input';
-export * from './Spinner'
